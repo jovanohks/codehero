2957cbbe551c2f6b37e357f9ddda89ac5ad7ce4d
jovanohks <root@jovan-pc.localdomain>
2024-09-06 20:28:44 +0800

    changed the code after code hero.

--- codehero-ae1.py
+++ codehero-ae1.py
@@ -2,6 +2,6 @@ test_scores = [61, 31, 30, 51, 36, 46, 72, 46, 53, 52, 50, 67, 39, 41, 54, 61, 5
 
 test_participants = ["Alice", "Bob", "Charlie", "Diana", "Edward", "Fiona", "George", "Hannah", "Ian", "Jane", "Kevin", "Laura", "Michael", "Nina", "Oliver", "Paula", "Quincy", "Rachel", "Samuel", "Tina", "Uma", "Victor", "Wendy", "Xavier", "Yvonne", "Zachary", "Adam", "Beth", "Carl", "Daisy", "Ethan", "Faith", "Gavin", "Helen", "Isaac", "Julia", "Kyle", "Lily", "Martin", "Nora", "Oscar", "Penelope", "Quinn", "Ruth", "Scott", "Tracy", "Ursula", "Vincent", "Willow", "Xander"] 
 
-for i in range(1,50):
+for i in range(1,len(test_scores)):
   if test_scores[i] <= 40:
-    print(test_participants[i])
+    print(str(test_participants[i]) +"\t"+ str(test_scores[i]))
